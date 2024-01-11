f265033c94235de46620c8bee1b17451bd85c6bb
leehojin <dlghwls0203@naver.com>
2024-01-11 14:28:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/brandol/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.brandol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.brandol.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomnavigation()
+    }
+
+    private fun initBottomnavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.page_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.page_point -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, PointFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.page_abata -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, AbataFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.page_mypage -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MypageFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
+}
