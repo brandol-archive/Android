4cc5b25dad19fefaea1d4cd335535a1ed9935297
jihyun <hjh20030615@gmail.com>
2024-01-21 14:31:54 +0900

    page2/brandlist 부분

---
+++ app/src/main/java/com/example/brandol/LinktreeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.brandol
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class LinktreeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
