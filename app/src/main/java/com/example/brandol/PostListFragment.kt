a524a42d45682104d055baeebbcb292f39369df5
leehojin <dlghwls0203@naver.com>
2024-01-21 15:26:58 +0900

    feat: 10페이지 탭 레이아웃 작성한 글 까지 구현

--- app/src/main/java/com/example/brandol/PostListFragment.kt
+++ app/src/main/java/com/example/brandol/PostListFragment.kt
@@ -5,55 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.brandol.databinding.FragmentPostListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PostListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PostListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding: FragmentPostListBinding
+    private var postList = ArrayList<Post>()
+    private var postAdapter = PostRVAdapter(postList)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentPostListBinding.inflate(inflater,container,false)
+        postList.apply {
+            add(Post("게시물 이름","게시물 내용","게시판 이름"))
+            add(Post("게시물 이름","게시물 내용","게시판 이름"))
+            add(Post("게시물 이름","게시물 내용","게시판 이름"))
+            add(Post("게시물 이름","게시물 내용","게시판 이름"))
+            add(Post("게시물 이름","게시물 내용","게시판 이름"))
+            add(Post("게시물 이름","게시물 내용","게시판 이름"))
+            add(Post("게시물 이름","게시물 내용","게시판 이름"))
+        }
+        binding.postListRv.adapter = postAdapter
+        binding.postListRv.layoutManager = LinearLayoutManager(activity)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_list, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/example/brandol/PostRVAdapter.kt
+++ app/src/main/java/com/example/brandol/PostRVAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.brandol
 
-class PostRVAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PostRVAdapter(private val postList: List<Post>) :
+    RecyclerView.Adapter<PostRVAdapter.PostViewHolder>() {
+
+    inner class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.item_post_name_tv)
+        val contents: TextView = itemView.findViewById(R.id.item_post_contents_tv)
+        val noticeBoard: TextView = itemView.findViewById(R.id.item_post_notice_board_tv)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
+        return PostViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.name.text = postList.get(position).name
+        holder.contents.text = postList.get(position).contents
+        holder.noticeBoard.text = postList.get(position).noticeBoard
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
 }
