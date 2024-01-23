73f7a7abb1ed230ba689fbefab53356a362fd96a
imkyeongjin <dlarudwls098@gmail.com>
2024-01-24 00:48:19 +0900

    fix : 오류 수정

--- app/src/main/java/com/example/brandol/AvartarFragment.kt
+++ app/src/main/java/com/example/brandol/AvartarFragment.kt
@@ -6,41 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.brandol.databinding.FragmentAbataBinding
-
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class AvartarFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
     lateinit var binding: FragmentAbataBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAbataBinding.inflate(inflater,container,false)
-        //binding.abataChattingQuantity.bringToFront()
+
         return binding.root
     }
 
-    companion object {
-
-        fun newInstance(param1: String, param2: String) =
-            AvartarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
