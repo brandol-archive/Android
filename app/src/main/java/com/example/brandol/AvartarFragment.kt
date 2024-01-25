998e1944775b8f13aac42504242a0d0a3e857367
leehojin <dlghwls0203@naver.com>
2024-01-25 20:58:02 +0900

    feat: conflict 수정

--- app/src/main/java/com/example/brandol/AvartarFragment.kt
+++ app/src/main/java/com/example/brandol/AvartarFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.example.brandol.databinding.FragmentAbataBinding
 
 import androidx.fragment.app.Fragment
 import com.example.brandol.databinding.FragmentAvartarBinding
