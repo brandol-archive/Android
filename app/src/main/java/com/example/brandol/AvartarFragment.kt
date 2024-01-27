af4f22e548759ed53d53996c63d44ebfc7bac977
imkyeongjin <dlarudwls098@gmail.com>
2024-01-27 19:01:07 +0900

    fix : 충돌로 인한 코드 수정

--- app/src/main/java/com/example/brandol/AvartarFragment.kt
+++ app/src/main/java/com/example/brandol/AvartarFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-//import com.example.brandol.databinding.FragmentAbataBinding
+import com.example.brandol.databinding.FragmentAbataBinding
 
 import androidx.fragment.app.Fragment
 import com.example.brandol.databinding.FragmentAvartarBinding
