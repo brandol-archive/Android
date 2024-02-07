3607a7031345576cfddc916839297ffbc288a33f
LeeHojin <141011143+LeeHojin165@users.noreply.github.com>
2024-02-07 12:28:00 +0900

    Revert "fix : 메니페스트 파일 수정"

--- app/src/main/java/com/example/brandol/AvatarStoreItemAdapter.kt
+++ app/src/main/java/com/example/brandol/AvatarStoreItemAdapter.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
+
 class AvatarStoreItemAdapter : ListAdapter<ItemModel, AvatarStoreItemAdapter.ViewHolder>(ItemModelDiffCallback()) {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
