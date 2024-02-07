8d07b8ee3faeeb3719d8b8525f19c09bb91de170
Hongji03 <aster030th@naver.com>
2024-02-07 19:44:16 +0900

    fix: brandinfo 및 board 관련 파일 수정

--- app/src/main/java/com/example/brandol/LinktreeFragment.kt
+++ app/src/main/java/com/example/brandol/LinktreeFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.brandol.databinding.FragmentLinktreeBinding
 
 class LinktreeFragment : Fragment() {
+
+    lateinit var binding: FragmentLinktreeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val view = FragmentLinktreeBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 }
