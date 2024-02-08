944d5cca009d379ecaa90492bf4f195204086662
SeohyeonJung <jung0321p@naver.com>
2024-02-08 21:18:00 +0900

    [feat] page4/page7/page11 화면 전환 구현 완료

--- app/src/main/java/com/example/brandol/PointMission1Fragment.kt
+++ app/src/main/java/com/example/brandol/PointMission1Fragment.kt
@@ -1,4 +1,32 @@
 package com.example.brandol
 
-class PointMission1Fragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.brandol.databinding.FragmentPointDetailBinding
+import com.example.brandol.databinding.FragmentPointMission1Binding
+
+class PointMission1Fragment: Fragment() {
+
+    private lateinit var binding: FragmentPointMission1Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPointMission1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 뒤로가기 버튼 클릭 시 Fragment를 닫습니다.
+        binding.pointMissionBackBtnIv.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+    }
 }
