45d4c20bb8090d1923ffb2b724aa6a6822c78446
ImKyungJin <dlarudwls098@gmail.com>
2024-02-12 20:28:06 +0900

    feat: 포인트 미션 중 설문조사형 최종 구현 완료

--- app/src/main/java/com/example/brandol/PointMissionSurveyFragment.kt
+++ app/src/main/java/com/example/brandol/PointMissionSurveyFragment.kt
@@ -1,8 +1,14 @@
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
+import com.example.brandol.R
+import com.example.brandol.collection.ItemModel
 import com.example.brandol.databinding.FragmentPointMissionSurveyBinding
 
 class PointMissionSurveyFragment : Fragment() {
@@ -25,6 +31,11 @@ class PointMissionSurveyFragment : Fragment() {
             requireActivity().supportFragmentManager.popBackStack()
         }
 
+        // 미션 완료 성공시 뜨는 팝업
+        binding.pointMissionSurveyCompleteIb.setOnClickListener {
+            showDialog()
+        }
+
         // Q1 문제에 대한 체크 상태 변경 리스너
         binding.q1MissionO.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -40,11 +51,29 @@ class PointMissionSurveyFragment : Fragment() {
             updateCompletionMessageVisibility()
         }
 
+        // Q2 문제에 대한 체크 상태 변경 리스너
+        binding.q2MissionO.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q2MissionX.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q2MissionX.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q2MissionO.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+
+
         // Q3 문제에 대한 체크 상태 변경 리스너
         binding.q31stB.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 binding.q32ndB.isChecked = false
                 binding.q33rdB.isChecked = false
+                binding.q34thB.isChecked = false
             }
             updateCompletionMessageVisibility()
         }
@@ -53,6 +82,7 @@ class PointMissionSurveyFragment : Fragment() {
             if (isChecked) {
                 binding.q31stB.isChecked = false
                 binding.q33rdB.isChecked = false
+                binding.q34thB.isChecked = false
             }
             updateCompletionMessageVisibility()
         }
@@ -61,6 +91,161 @@ class PointMissionSurveyFragment : Fragment() {
             if (isChecked) {
                 binding.q32ndB.isChecked = false
                 binding.q31stB.isChecked = false
+                binding.q34thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q34thB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q32ndB.isChecked = false
+                binding.q31stB.isChecked = false
+                binding.q33rdB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+
+        // Q4 문제에 대한 체크 상태 변경 리스너
+        binding.q41stB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q42ndB.isChecked = false
+                binding.q43rdB.isChecked = false
+                binding.q44thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q42ndB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q41stB.isChecked = false
+                binding.q43rdB.isChecked = false
+                binding.q44thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q43rdB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q42ndB.isChecked = false
+                binding.q41stB.isChecked = false
+                binding.q44thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q44thB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q42ndB.isChecked = false
+                binding.q41stB.isChecked = false
+                binding.q43rdB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+
+
+        // Q5 문제에 대한 체크 상태 변경 리스너
+        binding.q51stB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q52ndB.isChecked = false
+                binding.q53rdB.isChecked = false
+                binding.q54thB.isChecked = false
+                binding.q55thB.isChecked = false
+                binding.q56thB.isChecked = false
+                binding.q57thB.isChecked = false
+                binding.q58thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q52ndB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q51stB.isChecked = false
+                binding.q53rdB.isChecked = false
+                binding.q54thB.isChecked = false
+                binding.q55thB.isChecked = false
+                binding.q56thB.isChecked = false
+                binding.q57thB.isChecked = false
+                binding.q58thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q53rdB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q51stB.isChecked = false
+                binding.q52ndB.isChecked = false
+                binding.q54thB.isChecked = false
+                binding.q55thB.isChecked = false
+                binding.q56thB.isChecked = false
+                binding.q57thB.isChecked = false
+                binding.q58thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q54thB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q51stB.isChecked = false
+                binding.q52ndB.isChecked = false
+                binding.q53rdB.isChecked = false
+                binding.q55thB.isChecked = false
+                binding.q56thB.isChecked = false
+                binding.q57thB.isChecked = false
+                binding.q58thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q55thB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q51stB.isChecked = false
+                binding.q52ndB.isChecked = false
+                binding.q53rdB.isChecked = false
+                binding.q54thB.isChecked = false
+                binding.q56thB.isChecked = false
+                binding.q57thB.isChecked = false
+                binding.q58thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q56thB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q51stB.isChecked = false
+                binding.q52ndB.isChecked = false
+                binding.q53rdB.isChecked = false
+                binding.q54thB.isChecked = false
+                binding.q55thB.isChecked = false
+                binding.q57thB.isChecked = false
+                binding.q58thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q57thB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q51stB.isChecked = false
+                binding.q52ndB.isChecked = false
+                binding.q53rdB.isChecked = false
+                binding.q54thB.isChecked = false
+                binding.q55thB.isChecked = false
+                binding.q56thB.isChecked = false
+                binding.q58thB.isChecked = false
+            }
+            updateCompletionMessageVisibility()
+        }
+
+        binding.q58thB.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.q51stB.isChecked = false
+                binding.q52ndB.isChecked = false
+                binding.q53rdB.isChecked = false
+                binding.q54thB.isChecked = false
+                binding.q55thB.isChecked = false
+                binding.q56thB.isChecked = false
+                binding.q57thB.isChecked = false
             }
             updateCompletionMessageVisibility()
         }
@@ -71,13 +256,13 @@ class PointMissionSurveyFragment : Fragment() {
 
     private fun updateCompletionMessageVisibility() {
         if (areAllQuestionsAnswered()) {
-            binding.pointMissionSurveyBeforeIb.visibility = View.VISIBLE
-            binding.pointMissionSurveyBeforeTv.visibility = View.VISIBLE
+            binding.pointMissionSurveyCompleteIb.visibility = View.VISIBLE
+            binding.pointMissionSurveyCompleteTv.visibility = View.VISIBLE
             binding.pointMissionSurveyIngIb.visibility = View.GONE
             binding.pointMissionSurveyIngTv.visibility = View.GONE
         } else {
-            binding.pointMissionSurveyBeforeIb.visibility = View.GONE
-            binding.pointMissionSurveyBeforeTv.visibility = View.GONE
+            binding.pointMissionSurveyCompleteIb.visibility = View.GONE
+            binding.pointMissionSurveyCompleteTv.visibility = View.GONE
             binding.pointMissionSurveyIngIb.visibility = View.VISIBLE
             binding.pointMissionSurveyIngTv.visibility = View.VISIBLE
         }
@@ -85,13 +270,47 @@ class PointMissionSurveyFragment : Fragment() {
 
     private fun areAllQuestionsAnswered(): Boolean {
         val isQ1Answered = binding.q1MissionO.isChecked || binding.q1MissionX.isChecked
-        //val isQ2Answered = /* Add your check for Q2, for example: */ !binding.q2Edittext.text.isNullOrEmpty()
-        val isQ3Answered = binding.q31stB.isChecked || binding.q32ndB.isChecked || binding.q33rdB.isChecked
+        val isQ2Answered = binding.q2MissionO.isChecked || binding.q2MissionX.isChecked
+        val isQ3Answered = binding.q31stB.isChecked || binding.q32ndB.isChecked || binding.q33rdB.isChecked || binding.q34thB.isChecked
+        val isQ4Answered = binding.q41stB.isChecked || binding.q42ndB.isChecked || binding.q43rdB.isChecked || binding.q44thB.isChecked
+        val isQ5Answered = binding.q51stB.isChecked || binding.q52ndB.isChecked || binding.q53rdB.isChecked || binding.q54thB.isChecked || binding.q55thB.isChecked || binding.q56thB.isChecked || binding.q57thB.isChecked || binding.q58thB.isChecked
+        val isQ6Answered = !binding.q6Edittext.text.isNullOrEmpty()
+        val isQ7Answered = !binding.q7Edittext.text.isNullOrEmpty()
+        val isQ8Answered = !binding.q8Edittext.text.isNullOrEmpty()
+        val isQ9Answered = !binding.q9Edittext.text.isNullOrEmpty()
+        val isQ10Answered = !binding.q10Edittext.text.isNullOrEmpty()
 
         // Add similar checks for other questions...
 
-        //return isQ1Answered && isQ2Answered && isQ3Answered /* Add other question checks... */
-        return isQ1Answered  && isQ3Answered
+        return isQ1Answered && isQ2Answered && isQ3Answered && isQ4Answered && isQ5Answered && isQ6Answered && isQ7Answered && isQ8Answered && isQ9Answered && isQ10Answered  /* Add other question checks... */
+    }
+
+    private fun showDialog() {
+        val context = requireContext()
+
+        val dialog = Dialog(context)
+        dialog.setContentView(LayoutInflater.from(context).inflate(R.layout.dialog_mission_complete_popup, null))
+        //dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        // Set custom dialog size
+        val width = resources.displayMetrics.widthPixels * 0.7 // Adjust as needed
+        val height = resources.displayMetrics.heightPixels * 0.5 // Adjust as needed
+        dialog.window?.setLayout(width.toInt(), height.toInt())
+
+        // Make dialog background transparent
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+
+        dialog.show()
+
+//        // 다이얼로그를 직접 생성하고 커스텀 레이아웃 설정
+//        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_mission_complete_popup, null)
+//        val dialog = AlertDialog.Builder(context)
+//            .setView(dialogView)
+//            .create()
+//
+//        // 다이얼로그 표시
+//        dialog.show()
     }
 
 }
